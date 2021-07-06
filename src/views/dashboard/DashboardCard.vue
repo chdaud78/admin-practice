5fcc10e24caef02627e837ac78613961b6c38aa3
chdaud78 <qkrri56@naver.com>
2021-07-06 16:42:04 +0900

    [ENH] Add Chart

--- src/views/dashboard/DashboardCard.vue
+++ src/views/dashboard/DashboardCard.vue
@@ -9,6 +9,7 @@
                                 <div class="card-title text-value-lg">{{ card.title }}</div>
                                 <div class="card-subtitle" v-html="card.subtitle"></div>
                             </div>
+                            <vue3-chart-js v-bind="{...Chart}"></vue3-chart-js>
                         </div>
                     </div>
                 </div>
@@ -18,8 +19,13 @@
 </template>
 
 <script>
+import Vue3ChartJs from '@j-t-mcc/vue3-chartjs'
+
 export default {
     name: "DashboardCard",
+    components: {
+        Vue3ChartJs,
+    },
     data() {
         return {
             cards: [
@@ -45,6 +51,50 @@ export default {
                 },
             ]
         };
+    },
+    setup() {
+        const Chart = {
+            id: 'chart',
+            type: 'line',
+            data: {
+                labels: ["January","February","March","April","May","June","July"],
+                datasets: [
+                    {
+                        label: "My First dataset",
+                        data: [65, 59, 84, 84, 51, 55, 40],
+                        backgroundColor: "#877EDB",
+                        borderColor: "#877EDB",
+                        borderWidth: 1,
+                        tension: 0.5,
+                        pointStyle: 'circle',
+                        pointBorderColor: '#877EDB',
+                        pointBackgroundColor: "inherit",
+                    }
+                ],
+            },
+            options: {
+                responsive: true,
+                scales: {
+                    x: {
+                        display: false,
+                    },
+                    y: {
+                        display: false,
+                        min: 30,
+                        max: 120,
+                    },
+                },
+                plugins: {
+                    legend: {
+                        display: false,
+                    }
+                }
+            }
+        }
+
+        return {
+            Chart,
+        }
     },
 }
 </script>

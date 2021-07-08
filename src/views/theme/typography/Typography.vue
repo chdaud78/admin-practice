07a5a4b468bd9251f194b635ee35e171d9106e9b
chdaud78 <qkrri56@naver.com>
2021-07-08 16:24:31 +0900

    [ENH] Update typography page

---
+++ src/views/theme/typography/Typography.vue
@@ -0,0 +1,186 @@
+<template>
+    <div class="typography">
+        <div class="card">
+            <div class="card-header">Headings</div>
+            <div class="card-body">
+                <p>Typography 예제</p>
+                <table class="table">
+                    <thead>
+                        <tr>
+                            <th>Heading</th>
+                            <th>Example</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td>
+                                <code>
+                                    &lt;h1&gt;&lt;/h1&gt;
+                                </code>
+                            </td>
+                            <td>
+                                <h1>
+                                    h1.Bootstrap heading
+                                </h1>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td>
+                                <code>
+                                    &lt;h2&gt;&lt;/h2&gt;
+                                </code>
+                            </td>
+                            <td>
+                                <h2>
+                                    h2.Bootstrap heading
+                                </h2>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td>
+                                <code>
+                                    &lt;h3&gt;&lt;/h3&gt;
+                                </code>
+                            </td>
+                            <td>
+                                <h3>
+                                    h3.Bootstrap heading
+                                </h3>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td>
+                                <code>
+                                    &lt;h4&gt;&lt;/h4&gt;
+                                </code>
+                            </td>
+                            <td>
+                                <h4>
+                                    h4.Bootstrap heading
+                                </h4>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td>
+                                <code>
+                                    &lt;h5&gt;&lt;/h5&gt;
+                                </code>
+                            </td>
+                            <td>
+                                <h5>
+                                    h5.Bootstrap heading
+                                </h5>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td>
+                                <code>
+                                    &lt;h6&gt;&lt;/h6&gt;
+                                </code>
+                            </td>
+                            <td>
+                                <h6>
+                                    h6.Bootstrap heading
+                                </h6>
+                            </td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+        </div>
+
+        <div class="card">
+            <div class="card-header">Headings</div>
+            <div class="card-body">
+                <p>
+                    <code>.h1</code>과 <code>.h2</code> 클래스도 이용가능하지만 관련 HTML elements는 이용 불가
+                </p>
+                <p class="h1">h1. Bootstrap heading</p>
+                <p class="h2">h2. Bootstrap heading</p>
+                <p class="h3">h3. Bootstrap heading</p>
+                <p class="h4">h4. Bootstrap heading</p>
+                <p class="h5">h5. Bootstrap heading</p>
+                <p class="h6">h6. Bootstrap heading</p>
+            </div>
+        </div>
+
+        <div class="card">
+            <div class="card-header">Headings</div>
+            <div class="card-body">
+                <p>
+                    <code>.h1</code>과 <code>.h2</code> 클래스도 이용가능하지만 관련 HTML elements는 이용 불가
+                </p>
+                <p class="h1">h1. Bootstrap heading</p>
+                <p class="h2">h2. Bootstrap heading</p>
+                <p class="h3">h3. Bootstrap heading</p>
+                <p class="h4">h4. Bootstrap heading</p>
+                <p class="h5">h5. Bootstrap heading</p>
+                <p class="h6">h6. Bootstrap heading</p>
+            </div>
+        </div>
+
+        <div class="card">
+            <div class="card-header">Display Headings</div>
+            <div class="card-body">
+                <p>
+                    눈에 띄는 heading을 원한다면 이용을 권유.
+                </p>
+                <table class="table">
+                    <tbody>
+                    <tr>
+                        <td>
+                            <p class="display-1">Display 1</p>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>
+                            <p class="display-2">Display 2</p>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>
+                            <p class="display-3">Display 3</p>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>
+                            <p class="display-4">Display 4</p>
+                        </td>
+                    </tr>
+                    </tbody>
+                </table>
+            </div>
+        </div>
+
+        <div class="card">
+            <div class="card-header">Inline text elements</div>
+            <div class="card-body">
+                <p>
+                    <del>삭제된 글이라는 뜻입니다.</del>
+                </p>
+                <p>
+                    <s>더이상 정확하지 않다는 뜻입니다.</s>
+                </p>
+                <p>
+                    <ins>문서에 추가된 글이라는 뜻입니다.</ins>
+                </p>
+                <p>
+                    <u>밑줄 추가된 글입니다.</u>
+                </p>
+                <p>
+                    <small>미세한 글</small>
+                </p>
+                <p>
+                    <strong>볼드체</strong>
+                </p>
+                <p>
+                    <em>이텔릭 체</em>
+                </p>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+
+</script>

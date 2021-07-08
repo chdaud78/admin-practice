02595faf93b68a9a38138ad6a195db8f0593033e
chdaud78 <qkrri56@naver.com>
2021-07-08 13:45:42 +0900

    [ENH] Update webhistory for vue

--- src/router/index.js
+++ src/router/index.js
@@ -1,4 +1,4 @@
-import { createRouter, createWebHistory } from "vue-router";
+import { createRouter, createWebHashHistory } from "vue-router";
 import Dashboard from "@/router/dashboard/index"
 
 const routes = [
@@ -6,7 +6,7 @@ const routes = [
 ];
 
 const router = createRouter({
-    history: createWebHistory(process.env.BASE_URL),
+    history: createWebHashHistory(process.env.BASE_URL),
     historyApiFallback: true,
     routes,
 });

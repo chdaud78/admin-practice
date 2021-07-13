bf8048237adb78c2338af277bd125ffb77eb761f
chdaud78 <qkrri56@naver.com>
2021-07-13 14:38:46 +0900

    [ENH] Update tabClass

--- src/views/components/base/tabs/TabsPage.vue
+++ src/views/components/base/tabs/TabsPage.vue
@@ -1,31 +1,31 @@
 <template>
     <div class="tabs-page">
         <div class="row">
-            <template v-for="tabClass in tabClasses">
+            <template v-for="(tabClass, index) in tabClasses">
                 <div class="col-md-6 mb-4">
                     <div class="nav-tabs-boxed" :class="tabClass.class">
                         <ul class="nav nav-tabs" role="tablist">
                             <li class="nav-item">
-                                <a class="nav-link" data-toggle="tab" href="#home-1" role="tab" aria-controls="home"
+                                <a class="nav-link" data-toggle="tab" :href="`#home-${index}`" role="tab" aria-controls="home"
                                    aria-selected="false">
                                     Home
                                 </a>
                             </li>
                             <li class="nav-item">
-                                <a class="nav-link" data-toggle="tab" href="#profile-1" role="tab"
+                                <a class="nav-link" data-toggle="tab" :href="`#profile-${index}`" role="tab"
                                    aria-controls="profile" aria-selected="false">
                                     Profile
                                 </a>
                             </li>
                             <li class="nav-item">
-                                <a class="nav-link active" data-toggle="tab" href="#messages-1" role="tab"
+                                <a class="nav-link active" data-toggle="tab" :href="`#messages-${index}`" role="tab"
                                    aria-controls="messages" aria-selected="true">
                                     Messages
                                 </a>
                             </li>
                         </ul>
                         <div class="tab-content">
-                            <div class="tab-pane" id="home-1" role="tabpanel">
+                            <div class="tab-pane" :id="`home-${index}`" role="tabpanel">
                                 1. Lorem ipsum dolor sit amet, consectetur
                                 adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
                                 enim
@@ -35,7 +35,7 @@
                                 fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui
                                 officia deserunt mollit anim id est laborum.
                             </div>
-                            <div class="tab-pane" id="profile-1" role="tabpanel">
+                            <div class="tab-pane" :id="`profile-${index}`" role="tabpanel">
                                 2. Lorem ipsum dolor sit amet, consectetur
                                 adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
                                 enim
@@ -45,7 +45,7 @@
                                 fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui
                                 officia deserunt mollit anim id est laborum.
                             </div>
-                            <div class="tab-pane active" id="messages-1" role="tabpanel">
+                            <div class="tab-pane active" :id="`messages-${index}`" role="tabpanel">
                                 3. Lorem ipsum dolor sit amet,
                                 consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna
                                 aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut

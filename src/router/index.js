9aad51c078fbb5de843871d2c5823d144a965107
chdaud78 <qkrri56@naver.com>
2021-07-13 11:33:24 +0900

    [BLD] npm run build

--- src/router/index.js
+++ src/router/index.js
@@ -1,4 +1,4 @@
-import { createRouter, createWebHistory } from "vue-router";
+import { createRouter, createWebHashHistory } from "vue-router";
 import Dashboard from "@/router/dashboard/index"
 import Theme from "@/router/theme/index"
 import Components from "@/router/components/index"
@@ -10,7 +10,7 @@ const routes = [
 ];
 
 const router = createRouter({
-    history: createWebHistory(process.env.BASE_URL),
+    history: createWebHashHistory(process.env.BASE_URL),
     historyApiFallback: true,
     routes,
 });

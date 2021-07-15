f08f38fdf0bb58d55b3d979cd5516f7ff31f8027
chdaud78 <qkrri56@naver.com>
2021-07-15 14:42:25 +0900

    [ENH] Update alerts page

--- src/views/components/notifications/alerts/Alerts.vue
+++ src/views/components/notifications/alerts/Alerts.vue
@@ -4,11 +4,67 @@
             <div class="col-sm-6">
                 <div class="card">
                     <div class="card-header">Bootstrap Alert</div>
+                    <div class="card-body">
+                        <div class="alert alert-primary">A simple primary alert</div>
+                        <div class="alert alert-secondary">A simple secondary alert</div>
+                        <div class="alert alert-success">A simple success alert</div>
+                        <div class="alert alert-danger">A simple danger alert</div>
+                        <div class="alert alert-warning">A simple warning alert</div>
+                        <div class="alert alert-info">A simple info alert</div>
+                        <div class="alert alert-light">A simple light alert</div>
+                        <div class="alert alert-dark">A simple dark alert</div>
+                    </div>
                 </div>
             </div>
             <div class="col-sm-6">
                 <div class="card">
-                    <div class="card-header">Bootstrap Alert <small class="text-muted">suse</small></div>
+                    <div class="card-header">Bootstrap Alert <small class="text-muted">use
+                        <code>.alert-link</code></small></div>
+                    <div class="card-body">
+                        <div class="alert alert-primary">A simple primary alert <a href="#" class="alert-link">Link</a>
+                        </div>
+                        <div class="alert alert-secondary">A simple secondary alert <a href="#"
+                                                                                       class="alert-link">Link</a></div>
+                        <div class="alert alert-success">A simple success alert <a href="#" class="alert-link">Link</a>
+                        </div>
+                        <div class="alert alert-danger">A simple danger alert <a href="#" class="alert-link">Link</a>
+                        </div>
+                        <div class="alert alert-warning">A simple warning alert <a href="#" class="alert-link">Link</a>
+                        </div>
+                        <div class="alert alert-info">A simple info alert <a href="#" class="alert-link">Link</a></div>
+                        <div class="alert alert-light">A simple light alert <a href="#" class="alert-link">Link</a>
+                        </div>
+                        <div class="alert alert-dark">A simple dark alert <a href="#" class="alert-link">Link</a></div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-sm-6">
+                <div class="card">
+                    <div class="card-header">Bootstrap Alerts <small class="text-muted">additional content</small></div>
+                    <div class="card-body">
+                        <div class="alert alert-success">
+                            <h4>Well done!</h4>
+                            <p>Aww yeah,you successfully read this important alert message. This example text is going
+                                to run a bit longer so that you can see how spacing within an alert works with this kind
+                                of content.</p>
+                            <hr>
+                            <p class="mb-0">Whenever you need to, be sure to use margin utilities to keep things nice
+                                and tidy.</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-sm-6">
+                <div class="card">
+                    <div class="card-header">Bootstrap Alerts <small class="text-muted">dismissing</small></div>
+                    <div class="card-body">
+                        <div class="alert alert-warning alert-dismissible fade show" role="alert">
+                            <strong>Holy guacamole!</strong>
+                            You should check in on some of those fields below.
+                            <button class="close" type="button" data-dismiss="alert" aria-label="Close"><span
+                                aria-hidden="true">×</span></button>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
